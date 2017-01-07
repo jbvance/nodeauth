5a40d6f49776ff73c053c394c7d3d8399f053039
Jason Vance <jbvance@gmail.com>
2017-01-07 14:47:14 -0600

    Add user data model and user registration model

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -46,7 +48,22 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
   	console.log('Errors');
   	res.render('register', {errors: errors});
   } else{
-  	console.log('No Errors');
+  	var newUser = new User({
+  		name: name,
+  		email: email,
+  		password: password,
+  		profileimage: profileimage
+  	});
+
+  	User.createUser(newUser, function(err, user){
+  		if (err) throw err;
+  		console.log(user);
+  	});
+
+  	req.flash('success', 'You are now registered and can login');
+
+  	res.location('/');
+  	res.redirect('/');
   }
 
 });
